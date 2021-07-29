6af43416cfde91de544ee15680844720f3ded5e3
e0 <shan.huang@me.com>
2021-07-29 21:39:00 +0200

    feat: add image generation

--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -6,13 +6,52 @@
 
   let init
   let chart
+  let candlesDrawn = 0
+  let technicalIndicatorsDrawn = 0
+  let started = false
 
   onMount(async () => {
     ({ init } = (await import("klinecharts/index.blank.js")).default)
   })
 
-  $: if (timeseries && init) {
+  const generateImageIfDone = () => {
+    const finishedDrawing =
+      (
+        candlesDrawn / 2 === timeseries.length
+        && technicalIndicatorsDrawn / 10 === timeseries.length
+      ) || (
+        candlesDrawn  === timeseries.length
+        && technicalIndicatorsDrawn / 5 === timeseries.length
+      )
+
+    if (!finishedDrawing) {
+      return
+    }
+
+    setTimeout(() => {
+      const imageUrl = chart.getConvertPictureUrl(true, 'png')
+      const img = new Image();
+      img.src = imageUrl;
+      document.body.appendChild(img);
+      document.getElementById('chart').remove()
+    })
+  }
+  
+
+  $: if (init && timeseries && !started) {
+    started = true
     chart = init(`chart`, options)
+
+    chart.subscribeAction('drawCandle', () => {
+      candlesDrawn++
+      generateImageIfDone()
+    })
+
+    chart.subscribeAction('drawTechnicalIndicator', () => {
+      technicalIndicatorsDrawn++
+      generateImageIfDone()
+    })
+
 
     chart.createTechnicalIndicator("MA", false, { id: "candle_pane" })
     chart.overrideTechnicalIndicator({
@@ -30,7 +69,6 @@
     chart.applyNewData(timeseries.map(([open, high, low, close, volume, timestamp]) =>
       ({ open, high, low, close, volume, timestamp }))
     )
-
   }
 </script>
 
@@ -40,5 +78,6 @@
   #chart {
     width: 725px;
     height: 500px;
+    background: white;
   }
 </style>
