384cfbcd99eaa74d84538e6a187017655f39e5b1
e0 <shan.huang@me.com>
2021-08-02 01:20:37 +0200

    feat: use dark theme as default

--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte'
+  import { toPng } from 'html-to-image'
   import { page } from '$app/stores'
   import { default as options } from '../chart/options'
   import { uploadImage } from '../api'
@@ -11,12 +12,17 @@
   let candlesDrawn = 0
   let technicalIndicatorsDrawn = 0
   let started = false
+  let done = false
 
   onMount(async () => {
     ({ init } = (await import("klinecharts/index.blank.js")).default)
   })
 
   const generateImageIfDone = () => {
+    if (done) {
+      return 
+    }
+
     const finishedDrawing =
       (
         candlesDrawn / 2 === timeseries.length
@@ -29,9 +35,10 @@
     if (!finishedDrawing) {
       return
     }
+    done = true
 
     setTimeout(async () => {
-      const imageUrl = chart.getConvertPictureUrl(true, 'png')
+      const imageUrl = await toPng(document.getElementById('chart'))
       const img = new Image();
       img.src = imageUrl;
       document.body.appendChild(img);
@@ -62,7 +69,7 @@
       calcParams: [10, 20, 50],
     })
 
-    const height = 100
+    const height = 150
     chart.createTechnicalIndicator("VOL", false, { height })
     chart.overrideTechnicalIndicator({
       name: "VOL",
@@ -80,7 +87,7 @@
 <style>
   #chart {
     width: 725px;
-    height: 500px;
-    background: white;
+    height: 850px;
+    background: #000;
   }
 </style>
